0b2f7fca881139c6c2460bfff90199ac6e3247e7
JavierMorales0 <javiermoralesmelara@gmail.com>
2023-12-02 16:54:19 -0600

    Add preference add/remove functionality and limit check

--- src/components/domain/profile/preferences/ConfigPreferenceDialog.jsx
+++ src/components/domain/profile/preferences/ConfigPreferenceDialog.jsx
@@ -2,6 +2,7 @@ import { Dialog, DialogContent, DialogTitle } from "@mui/material";
 import PreferenceItem from "#/components/domain/profile/preferences/PreferenceItem";
 import { useMemo } from "react";
 import SemiBold14 from "#/components/shared/fonts/SemiBold14";
+import { addToast } from "#/stores/UIState.js";
 
 const ConfigPreferenceDialog = ({
   open,
@@ -15,6 +16,27 @@ const ConfigPreferenceDialog = ({
     return optionsList[0]?.group || "";
   }, [optionsList]);
 
+  const quantityPreferencesOnGroup = useMemo(() => {
+    return optionsList?.reduce((acc, option) => {
+      if (optionsActive?.includes(option?._id)) {
+        acc += 1;
+      }
+      return acc;
+    }, 0);
+  }, [optionsList, optionsActive]);
+
+  const handleAddOrRemovePreference = (item) => {
+    if (optionsActive?.includes(item?._id)) {
+      handleRemovePreference(item?._id);
+    } else {
+      if (quantityPreferencesOnGroup >= 3) {
+        addToast("Limite alcanzado", "error");
+        return;
+      }
+      handleAddPreference(item?._id);
+    }
+  };
+
   return (
     <Dialog open={open} onClose={onClose} PaperProps={{ sx: style.dialog }}>
       <DialogTitle>
@@ -23,13 +45,7 @@ const ConfigPreferenceDialog = ({
       <DialogContent sx={style?.content}>
         {optionsList?.map((item) => (
           <PreferenceItem
-            onClick={() => {
-              if (optionsActive?.includes(item?._id)) {
-                handleRemovePreference(item?._id);
-              } else {
-                handleAddPreference(item?._id);
-              }
-            }}
+            onClick={() => handleAddOrRemovePreference(item)}
             key={item?._id}
             id={item?._id}
             name={item?.name}

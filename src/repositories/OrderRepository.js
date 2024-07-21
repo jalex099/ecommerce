b09d8be74852a9ec65d9cacb950f8198391973e8
jvrmrls <javiermoralesmelara@gmail.com>
2024-07-20 21:46:10 -0600

    chore: Refactor OrderRepository.js for improved code readability and maintainability

--- src/repositories/OrderRepository.js
+++ src/repositories/OrderRepository.js
@@ -1,45 +1,45 @@
 import API from "#/repositories/API.js";
 
 const OrderRepository = () => {
-
   const getAllOrders = async () => {
     return await API.get("/orders", {
-      secure: true
+      secure: true,
     });
-  }
+  };
 
   const getOrdersPending = async () => {
     return await API.get("/orders/pendings", {
-      secure: true
+      secure: true,
     });
-  }
+  };
 
   const getOrder = async (id) => {
     return await API.get(`/orders/${id}`, {
       await: true,
+      secure: true,
     });
-  }
+  };
 
   const saveOrder = async (data) => {
     return await API.post("/orders", data, {
       await: true,
       secure: true,
     });
-  }
+  };
 
   const confirmPayment = async (data) => {
     return await API.post(`/orders/${data?.idTransaccion}/confirm`, data, {
       await: true,
     });
-  }
+  };
 
   return {
     getOrder,
     getAllOrders,
     getOrdersPending,
     saveOrder,
-    confirmPayment
-  }
-}
+    confirmPayment,
+  };
+};
 
 export default OrderRepository;
